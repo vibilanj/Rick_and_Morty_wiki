3c81f9fbb8ecb8484243438914373e73ed716479
Vibilan Jayanth <vibilan@firstindo.com>
2023-02-06 16:24:12 +0800

    mvp

---
+++ src/components/Filters/Filters.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Gender from './Category/Gender'
+import Species from './Category/Species'
+import Status from './Category/Status'
+
+const Filters = ({setSpecies, setGender, setStatus, setPageNumber}) => {
+  let clear = () => {
+    setSpecies("");
+    setGender("");
+    setStatus("");
+    setPageNumber("");
+    window.location.reload(false)
+  };
+  return (
+    <div className='col-lg-3 col-12 mb-5'>
+      <div className="text-center fw-bold fs-4 mb-2">Filter</div>
+      <div 
+        onClick={clear}
+        style={{cursor: "pointer"}} 
+        className="text-center text-primary text-decoration-underline mb-4"
+      >
+        Clear Filters
+      </div>
+
+      <div className="accordion" id="accordionExample">
+        <Status setStatus={setStatus} setPageNumber={setPageNumber} />
+        <Species setSpecies={setSpecies} setPageNumber={setPageNumber} />
+        <Gender setGender={setGender} setPageNumber={setPageNumber} />
+      </div>
+    </div>
+  )
+}
+
+export default Filters
